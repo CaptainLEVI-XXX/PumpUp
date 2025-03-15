d1a93b53215afe3cc18e5c0407cad01bdc1891b4
saurabh <saurabhyadav42399@gmail.com>
2025-03-15 05:32:48 +0530

    fix pipeline

---
+++ test/Test.t.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import {Test} from 'forge-std/Test.sol';
+
+
+ contract TestDoNothing is Test{
+    function setUp() public {}
+
+    function test_t() public pure{
+
+    }
+ }
